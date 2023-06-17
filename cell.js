179e9cca361aa9d1f7868a754556d0bef1b35e63
Дарья Губина <128888851+Dashhha8@users.noreply.github.com>
2023-06-17 05:47:14 +0400

    Update cell.js

--- cell.js
+++ cell.js
@@ -3,46 +3,41 @@ export class Cell {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     gridElement.append(cell);
+    /* сохранение х и у внутри класса */ 
     this.x = x;
     this.y = y;
   }
 
   linkTile(tile) {
-    tile.setXY(this.x, this.y);
+    tile.setXY(this.x, this.y);/* остановка координат плитки*/
     this.linkedTile = tile;
   }
 
-  unlinkTile() {
-    this.linkedTile = null;
-  }
+  unlinkTile() { /*перезапись ссылки привязанной плитки*/
+    this.linkedTile = null;}
 
-  isEmpty() {
-    return !this.linkedTile;
-  }
+  isEmpty() {/*true или false есть ли в ячейке плитка*/
+    return !this.linkedTile;} 
 
-  linkTileForMerge(tile) {
-    tile.setXY(this.x, this.y);
-    this.linkedTileForMerge = tile;
-  }
+  linkTileForMerge(tile) { 
+    tile.setXY(this.x, this.y);/*замена координат плитки на новые*/
+    this.linkedTileForMerge = tile;} /*сохранение ссылки на плитку*/
 
   unlinkTileForMerge() {
-    this.linkedTileForMerge = null;
-  }
+    this.linkedTileForMerge = null;}
 
   hasTileForMerge() {
-    return !!this.linkedTileForMerge;
-  }
+    return !!this.linkedTileForMerge;}
 
-  canAccept(newTile) {
+  canAccept(newTile) {/*может ли переместиться плитка на тек.ячейку (true или false)*/
     return (
-      this.isEmpty() ||
-      (!this.hasTileForMerge() && this.linkedTile.value === newTile.value)
-    );
+      this.isEmpty() || (!this.hasTileForMerge() && this.linkedTile.value === newTile.value)
+      );
   }
 
   mergeTiles() {
-    this.linkedTile.setValue(this.linkedTile.value + this.linkedTileForMerge.value);
-    this.linkedTileForMerge.removeFromDOM();
-    this.unlinkTileForMerge();
+    this.linkedTile.setValue(this.linkedTile.value + this.linkedTileForMerge.value);/*сумма двух плиток*/
+    this.linkedTileForMerge.removeFromDOM(); /*удаление второй плитки*/
+    this.unlinkTileForMerge(); /*отвязка второй плитки от ячейки*/
   }
 }
