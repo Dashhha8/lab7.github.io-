e05e675ba40a5c3ae9eea4469805664bb663d29a
Дарья Губина <128888851+Dashhha8@users.noreply.github.com>
2023-06-17 05:48:07 +0400

    Update tile.js

--- tile.js
+++ tile.js
@@ -2,26 +2,23 @@ export class Tile {
   constructor(gridElement) {
     this.tileElement = document.createElement("div");
     this.tileElement.classList.add("tile");
-    this.setValue(Math.random() > 0.5 ? 2 : 4);
+    this.setValue(Math.random() > 0.5 ? 2 : 4); /* значение начальных плиток рандомно 2 или 4 */
     gridElement.append(this.tileElement);
   }
 
   setValue(value) {
     this.value = value;
-    this.tileElement.textContent = value;
-    const bgLightness = 100 - Math.log2(value) * 9;
+    this.tileElement.textContent = value; /*добавление полученного рандома текстом */
+    const bgLightness = 100 - Math.log2(value) * 9; /* логика изменения тонов цвета */
     this.tileElement.style.setProperty("--bg-lightness", `${bgLightness}%`);
-    this.tileElement.style.setProperty("--text-lightness", `${bgLightness < 50 ? 90 : 10}%`);
   }
 
-  setXY(x, y) {
+  setXY(x, y) { /* изменение значений х и у внутри плитки на новые */
     this.x = x;
     this.y = y;
-    this.tileElement.style.setProperty("--x", x);
+    this.tileElement.style.setProperty("--x", x); /* css */
     this.tileElement.style.setProperty("--y", y);
   }
 
-  removeFromDOM() {
-    this.tileElement.remove();
-  }
+  removeFromDOM() {this.tileElement.remove();}
 }
